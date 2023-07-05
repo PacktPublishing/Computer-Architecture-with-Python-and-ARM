0b98e7b1b3684bb0937bacdbf534c53b49aeaf3b
jubit-packt <jubitp@packt.com>
2023-07-06 00:02:31 +0530

    Added code files

--- Chapter02/02-sum-of-first-n.py
+++ Chapter02/02-sum-of-first-n.py
@@ -1,3 +1,3 @@
-n = int(input("How many integers are you adding? ")) #This line was wrong in the file
+n = int(input("How many integers are you adding? "))
 s = n * (n + 1)/2
 print("Sum of ",n, " integers is",s) # We used double quotes " instead of single
